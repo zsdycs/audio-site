2f944e3f8b0e4b346cc09c1a31a60ce5ca4484fc
zsdycs <mail@lipk.org>
2019-03-11 13:34:39 +0800

    一些关于样式的修改

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -25,13 +25,65 @@ $(document).ready(function () {
       $(this).next().css("display","block")
     }
   })
+
+  // 排序选项列表
+  $('.ui.dropdown').dropdown();
+  $(document).on("click",".rank-a",function(){
+    // console.log($(this).text()) 
+    // ajax排序请求局部刷新audioList
+    
+    // 修改列表标题
+    $('.dropdown-title').html($(this).text())
+  })
+
+  
 });
 
-$('.ui.dropdown').dropdown();
-$(document).on("click",".rank-a",function(){
-  // console.log($(this).text()) 
-  // ajax排序请求局部刷新audioList
-  
-  // 修改列表标题
-  $('.dropdown-title').html($(this).text())
+
+
+// 分页
+// 请求获得最大分页
+var maxpage=11,nowpage=1
+isInsufficientPage()
+$(document).on("click",".paging",function(){
+  $(".paging").removeClass("active")
+  $(this).addClass("active")
+  nowpage = parseInt($(this).text())
 })
+// 分页→右
+$(document).on("click",".toendpage",function(){
+  if(parseInt($(".pageend").text()) < maxpage){
+    for(i=0; i<$(".paging").length; i++){
+      $(".paging").eq(i).text(parseInt($(".paging").eq(i).text()) +4)
+      if(parseInt($(".paging").eq(i).text()) == nowpage){
+        $(".paging").eq(i).addClass("active")
+      }else{
+        $(".paging").eq(i).removeClass("active")
+      }
+      if(parseInt($(".paging").eq(i).text()) > maxpage){
+        $(".paging").eq(i).css("display","none")
+      }
+    }
+  }
+})
+// 分页←左
+$(document).on("click",".tofistpage",function(){
+  if(parseInt($(".pagefist").text()) > 1){
+    $(".paging").css("display","block")
+    for(i=0; i<$(".paging").length; i++){
+      $(".paging").eq(i).text(parseInt($(".paging").eq(i).text()) -4)
+      if(parseInt($(".paging").eq(i).text()) == nowpage){
+        $(".paging").eq(i).addClass("active")
+      }else{
+        $(".paging").eq(i).removeClass("active")
+      }
+    }
+  }
+})
+function isInsufficientPage(){
+  for(i=0; i<$(".paging").length; i++){
+    if(parseInt($(".paging").eq(i).text()) > maxpage){
+      $(".paging").eq(i).css("display","none")
+    }
+  }
+}
