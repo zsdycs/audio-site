6ea966b631276b4b6dcb11402eb5a077e03ea3ce
zsdycs <mail@lipk.org>
2019-03-27 16:38:18 +0800

    整理node_modules

--- index.js
+++ index.js
@@ -53,11 +53,6 @@ app.use(session({
 // flash 中间件，用来显示通知
 app.use(flash())
 
-// 处理表单及文件上传的中间件
-// app.use(require('express-formidable')({
-//   uploadDir: path.join(__dirname, 'public/updata'), // 上传文件目录
-//   keepExtensions: true// 保留后缀
-// }))
 app.use(multipart())
 
 // 设置模板全局常量
