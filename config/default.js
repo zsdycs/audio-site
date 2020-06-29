0263e7c4ef7b7251618a79d84936643116f96d3a
zsdycs <mail@lipk.org>
2020-06-29 17:16:09 +0800

    更新依赖，修改 session 过期时间

--- config/default.js
+++ config/default.js
@@ -3,8 +3,8 @@ module.exports = {
   session: {
     secret: 'audiosite',
     key: 'audiosite',
-    maxAge: 2592000000
+    maxAge: 3600 // 过期时间，过期后 cookie 中的 session id 自动删除
   },
   // mongodb 连接
-  mongodb: 'mongodb://localhost:27017/gp_db'
+  mongodb: 'mongodb://localhost:27017/audio-site'
 }
